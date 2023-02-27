3b27b46e6b568f9a78fdff4cc7ad5b02722ed6ad
Arthur Galiano <arthurgali99@gmail.com>
2023-02-26 22:15:44 -0500

    First commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BullyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BullyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //ctor + dos tabas crea el contructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        // Conjunto de Datos
+        public DbSet<Category> Categories { get; set; }
+    }
+}
